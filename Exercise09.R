b4ae181d2d4e1765b01a77c1fa60c42a2e6b0f3b
Seth Parker <55992647+sethparker@users.noreply.github.com>
2019-11-25 12:46:32 -0500

    Complete

--- Exercise09.R
+++ Exercise09.R
@@ -1,7 +1,7 @@
 #######################################
 # QUESTION 1 #
 #######################################
-# Plots temp(F) and relative humidity(%) today in South Bend against one another
+# Plots temp(F) and relative humidity(%) from last night until right now in South Bend against one another
 
 SB_data <- read.csv("KSBN.csv")
 library(ggplot2)
@@ -13,3 +13,18 @@ ggplot(data = SB_data)+
 #######################################
 # QUESTION 2 #
 #######################################
+
+pop_data <- read.csv(file.choose())
+# Select data.txt
+
+ggplot(data = pop_data)+
+  geom_boxplot(mapping = aes(x = region, y = observations, fill = region))
+  
+ggplot((data = pop_data))+
+  geom_jitter(mapping = aes(x = region, y = observations, color = region, ))
+
+# WRITTEN ANSWER BIT HERE
+# These plots do tell different stories. The boxplot shows that the means are all rather similar, which one may think means the data sets are all relatively similar. However, looking at the scatter plot reveals that we are actually looking at 4 distinct types of distributions with very little in common  
+
+
+

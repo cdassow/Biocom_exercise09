cb0e41367605380a67426b6542111d4343c0136f
Grace Petrosini <gpetrosi@nd.edu>
2019-11-28 20:42:24 -0500

    trendline added

--- Ex9DataPlotting.R
+++ Ex9DataPlotting.R
@@ -1,9 +1,16 @@
 #Exercise 09 GAP
 #Nov 26 2019
 
-#load data
-data<-read.table("./Biocomp_exercise09/wineQualityReds.csv")
+#packages
+library(ggplot2)
+library(cowplot)
 
-#plot data
+# load data
+airbnb<-read.csv("./Biocom_exercise09/airbnb.csv")
 
+# plot data
+a=ggplot(data=airbnb, mapping=aes(x=reviews, y=overall_satisfaction))
+
+a+geom_point(color="blue", size = 3)+xlab("Number of Reviews")+ylab("Overall Satisfaction")+
+  theme_bw() + geom_smooth(method="lm")
 

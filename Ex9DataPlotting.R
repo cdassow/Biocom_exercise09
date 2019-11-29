0441b777ae846e4fa569a58e7b27a4fe4ddb2072
Grace Petrosini <gpetrosi@nd.edu>
2019-11-28 21:07:26 -0500

    done

--- Ex9DataPlotting.R
+++ Ex9DataPlotting.R
@@ -11,6 +11,24 @@ airbnb<-read.csv("./Biocom_exercise09/airbnb.csv")
 # plot data
 a=ggplot(data=airbnb, mapping=aes(x=reviews, y=overall_satisfaction))
 
-a+geom_point(color="blue", size = 3)+xlab("Number of Reviews")+ylab("Overall Satisfaction")+
+a+geom_point()+xlab("Number of Reviews")+ylab("Overall Satisfaction")+
   theme_bw() + geom_smooth(method="lm")
+#ignore warning messages, data has not been cleaned for null and zero values
 
+# Part 2, Data Visualization
+# load data
+compass<-read.table("./Biocom_exercise09/data.txt", sep = ",", header = T)
+
+# bar plot - displays mean of each direction/region
+ggplot(compass, aes(region, observations))+
+  geom_bar(aes(fill = region),fun.y = "mean", stat = "summary")+
+  theme_bw()
+
+# scatter plot - shows distribution of observations within the direction/region
+ggplot(compass, aes(x = region, y = observations))+
+  geom_jitter(aes(color = region))+
+  theme_bw()
+
+#The bar and scatter plots tell a different story because one 
+#summarizes the data while the other shows the distribution of
+#the observatons within their categories.
